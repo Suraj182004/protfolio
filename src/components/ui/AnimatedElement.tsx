281fec36b2b1653b20fbc44190c75f8157a72b6b
surajyaligar2004 <surajyaligar18@gmail.com>
2025-04-02 17:04:59 +0530

    ad

--- src/components/ui/AnimatedElement.tsx
+++ src/components/ui/AnimatedElement.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { ReactNode } from 'react';
 import { motion, MotionProps } from 'framer-motion';
 import { useScrollAnimation } from '@/lib/hooks/useScrollAnimation';
